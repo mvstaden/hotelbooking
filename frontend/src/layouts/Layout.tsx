fc968bc733a25903182e7bcee281e0edc8de26c8
Marius Van Staden <mvstaden91@gmail.com>
2024-10-18 14:57:02 +0200

    Added Register routing

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  );
+};
+export default Layout;
