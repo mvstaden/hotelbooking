065e1a0317091f7eac86086ff598485d48c87e4d
Marius Van Staden <mvstaden91@gmail.com>
2024-11-08 08:37:04 +0200

    Created register routes in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import userRoutes from "./routes/users";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express end point" });
-});
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("Server is running on localhost 7000");
